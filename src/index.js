587693247007ad2184436d964d2c80b0f4f6a4dc
navi5599 <ivkelava10@gmail.com>
2022-09-07 14:49:46 +0200

    added Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('d3f9ca6564994232a305cdad080686c9').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
